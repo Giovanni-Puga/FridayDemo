55e0e1c1a10a6082da4855c20e6f75224112f702
Giovanni Puga <kajeebajeen@gmail.com>
2021-06-14 20:39:28 -0700

    Added CILab implementation and test cases

--- src/main/java/CILabInterface.java
+++ src/main/java/CILabInterface.java
@@ -16,9 +16,9 @@ public interface CILabInterface {
 
     /**
      *  We define the usage of capitals in a word to be right when one of the following cases holds:
-     *  All letters in this word are capitals, like "USA".
-     *  All letters in this word are not capitals, like "leetcode".
-     *  Only the first letter in this word is capital, like "Google".
+     *  All letters in this word are capitals, like "USA". (True)
+     *  All letters in this word are not capitals, like "leetcode". (True)
+     *  Only the first letter in this word is capital, like "Google". (True)
      * @return  Given a string word, return true if the usage of capitals in it is right.
      */
 
--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -18,14 +18,87 @@ public class CILabTest {
         myString = null;
     }
 
+    //Empty String test
     @Test
     public void detectCapitalUseTest1() {
-        fail("Not yet implemented");
-
+        assertThrows(NullPointerException.class, ()-> {myString.detectCapitalUse();});
     }
+
+    //Null String test
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString = null;
+        assertThrows(NullPointerException.class, ()-> {myString.detectCapitalUse();});
+    }
+
+    //First character capital test
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("Apples");
+        assertEquals(true, myString.detectCapitalUse());
+    }
+
+    //All character uppercase test
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("APPLES");
+        assertEquals(true, myString.detectCapitalUse());
+    }
+
+    //All character lowercase test
+    @Test
+    public void detectCapitalUseTest5() {
+        myString.setString("apples");
+        assertEquals(true, myString.detectCapitalUse());
+    }
+
+    //Improper casing test - middle
+    @Test
+    public void detectCapitalUseTest6() {
+        myString.setString("apPles");
+        assertEquals(false, myString.detectCapitalUse());
+    }
+
+    //Improper casing test - first and last
+    @Test
+    public void detectCapitalUseTest7() {
+        myString.setString("AppleS");
+        assertEquals(false, myString.detectCapitalUse());
+    }
+
+    //Improper casing test - all upper except one
+    @Test
+    public void detectCapitalUseTest8() {
+        myString.setString("APPLeS");
+        assertEquals(false, myString.detectCapitalUse());
+    }
+
+    //Improper casing test - Every other character is uppercase
+    @Test
+    public void detectCapitalUseTest9() {
+        myString.setString("aPpLeS");
+        assertEquals(false, myString.detectCapitalUse());
+    }
+
+    //Test proper capitalization of string with spaces
+    @Test
+    public void detectCapitalUseTest10() {
+        myString.setString("Apples are good for you");
+        assertEquals(true, myString.detectCapitalUse());
+    }
+
+    //Test improper capitalization of string with spaces
+    @Test
+    public void detectCapitalUseTest11() {
+        myString.setString("Apples Are Good for You");
+        assertEquals(false, myString.detectCapitalUse());
+    }
+
+    //Test that numbers and symbols return true by default
+    @Test
+    public void detectCapitalUseTest12() {
+        myString.setString("!!!!!890 @pp1e5...");
+        assertEquals(true, myString.detectCapitalUse());
     }
 
 
